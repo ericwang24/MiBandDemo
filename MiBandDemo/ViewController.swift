4311debe551cec5f8d03c6787ba88170da091aef
WangGuoming <wangguoming@91yong.com>
2017-03-07 18:30:39 +0800

    提交工程文件

---
+++ MiBandDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiBandDemo
+//
+//  Created by Eric Wang on 17/3/7.
+//  Copyright © 2017 Eric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
